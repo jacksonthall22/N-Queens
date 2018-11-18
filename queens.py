04ed0569c0c53f847d07bf62b45ad72fca994640
Jackson Hall <25040219+jacksonthall22@users.noreply.github.com>
2018-11-17 22:17:56 -0500

    Changed get_size() and get_mode() from methods to functions

--- queens.py
+++ queens.py
@@ -2,65 +2,66 @@ class Board:
     def __init__(self):
         self.size = self.__get_size()
 
-    @staticmethod
-    def __get_size():
-        """Prompt for and return size of the board."""
 
-        size = input('Enter board size:\n>>> ')
+def get_size():
+    """Prompt for and return size of the board."""
 
-        # Validate input
-        is_valid = False
-        while not is_valid:
-            try:
-                # Try to convert size to int
-                size = int(size)
-            except ValueError:
-                size = input('Please enter an integer:\n>>> ')
+    size = input('Enter board size:\n>>> ')
+
+    # Validate input
+    is_valid = False
+    while not is_valid:
+        try:
+            # Try to convert size to int
+            size = int(size)
+        except ValueError:
+            # Non-int entered
+            size = input('Please enter an integer:\n>>> ')
+        else:
+            if not 1 <= size <= 26:
+                # Size of board out of range
+                size = input('Please enter an integer between 1 and 26:\n>>> ')
+            elif size in [2, 3]:
+                # No solutions for 2x2 or 3x3
+                size = input('There are no solutions for a board of size '
+                             '2 or 3. Please enter another integer between 1 and '
+                             '26:\n>>>')
             else:
-                if not 1 <= size <= 26:
-                    # Size of board out of range
-                    size = input('Please enter an integer between 1 and 26:\n>>> ')
-                elif size in [2, 3]:
-                    # No solutions for 2x2 or 3x3
-                    size = input('There are no solutions for a board of size '
-                                 '2 or 3. Please enter another integer between 1 and '
-                                 '26:\n>>>')
-                else:
-                    # Size is an int and in range
-                    is_valid = True
+                # Size is an int and in range
+                is_valid = True
 
-        return size
+    return size
 
-    @staticmethod
-    def __get_mode():
-        """Prompt for and return display mode."""
 
-        print('Choose from a mode below: ')
-        print('\t1: Show all solutions immediately')
-        print('\t2: Pause at each solution')
-        print('\t3: Visual execution - cycle queen placements and '
-              'pause at solutions')
-        print('\t4: Step-by-step - pause at every queen placement\n')
+def get_mode():
+    """Prompt for and return display mode."""
 
-        mode = input('>>> ')
+    print('Choose from a mode below: ')
+    print('\t1: Show all solutions immediately')
+    print('\t2: Pause at each solution')
+    print('\t3: Visual execution - cycle queen placements and '
+          'pause at solutions')
+    print('\t4: Step-by-step - pause at every queen placement\n')
+    mode = input('>>> ')
 
-        # Validate input
-        is_valid = False
-        while not is_valid:
-            try:
-                # Try to convert mode to int
-                mode = int(mode)
-            except ValueError:
+    # Validate input
+    is_valid = False
+    while not is_valid:
+        try:
+            # Try to convert mode to int
+            mode = int(mode)
+        except ValueError:
+            # Non-int entered
+            mode = input('Please enter 1, 2, 3, or 4:\n>>> ')
+        else:
+            if mode not in [1, 2, 3, 4]:
+                # Invalid mode entered
                 mode = input('Please enter 1, 2, 3, or 4:\n>>> ')
             else:
-                if mode not in [1, 2, 3, 4]:
-                    # Invalid mode entered
-                    mode = input('Please enter 1, 2, 3, or 4:\n>>> ')
-                else:
-                    # Mode is an int and in range
-                    is_valid = True
+                # Mode is an int and in range
+                is_valid = True
 
-        return mode
+    return mode
 
 
 def main():
